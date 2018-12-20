f401332edf7169911c132acf3400da56660ba3a2
zqy6666 <45995241+zqy6666@users.noreply.github.com>
2018-12-20 20:33:01 +0800

    Create util.js

---
+++ util.js
@@ -0,0 +1,10 @@
+// util.js
+function formatTime(time, format) {
+  let temp = '0000000000' + time
+  let len = format.length
+  return temp.substr(-len)
+}
+
+module.exports = {
+  formatTime: formatTime
+}
